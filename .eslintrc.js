3de7f30213852b2b60f62252e3edf908856e04a2
clovesss <1214488973@qq.com>
2022-09-15 18:22:51 +0800

    Finished login Pages update

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,8 +21,25 @@ module.exports = {
         "allowFirstLine": false
       }
     }],
+
+    //  处理 disallow self-closing on html void elements 问题
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    // over
+
+
     "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline":"off",
+    "vue/multiline-html-element-content-newline": "off",
     "vue/name-property-casing": ["error", "PascalCase"],
     "vue/no-v-html": "off",
     'accessor-pairs': 2,
@@ -47,7 +64,7 @@ module.exports = {
     'curly': [2, 'multi-line'],
     'dot-location': [2, 'property'],
     'eol-last': 2,
-    'eqeqeq': ["error", "always", {"null": "ignore"}],
+    'eqeqeq': ["error", "always", { "null": "ignore" }],
     'generator-star-spacing': [2, {
       'before': true,
       'after': true
