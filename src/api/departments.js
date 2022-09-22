0d78808a27860076a08842f9cf0e96ff97d302f6
clovesss <1214488973@qq.com>
2022-09-22 17:18:57 +0800

    新增部门功能完成

--- src/api/departments.js
+++ src/api/departments.js
@@ -13,10 +13,20 @@ export function getDepartments() {
  * @description 删除组织架构的数据
 */
 //
-// 接口是根据restful的规则设计的，删除 delete  新增 post  修改 put 获取 get
+// 接口是根据restful的规则设计的，删除业务 delete  新增业务 post  修改 put 获取 get
 export function delDepartments(id) {
   return request({
     url: `/company/department/${id}`,
     method: 'delete'
+  })
+}
+/**
+ * @description 新增数据
+ */
+export function addDepartments(data) {
+  return request({
+    url: '/company/department',
+    methods: 'post',
+    data
   })
 }
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -5,7 +5,7 @@
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
         <!-- 缺少treeNode -->
-        <tree-tools :tree-node="company" :is-root="true" />
+        <tree-tools :tree-node="company" :is-root="true" @addDepts="addDepts" />
         <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
@@ -16,23 +16,29 @@
             slot-scope="{ data }"
             :tree-node="data"
             @delDepts="getDepartments"
+            @addDepts="addDepts"
           />
         </el-tree>
       </el-card>
     </div>
+    <!-- 弹框组件 -->
+    <addDepts :show-dialog="showDialog" />
   </div>
 </template>
 
 <script>
 import treeTools from './components/tree-tools.vue'
+import addDepts from './components/add-dept.vue' // 引入点击新增后的弹框组件
 import { getDepartments } from '@/api/departments.js'
 import { tranListToTreeData } from '@/utils'
 export default {
   components: {
-    treeTools
+    treeTools,
+    addDepts
   },
   data() {
     return {
+      showDialog: false, // 控制弹层的显示隐藏
       company: { name: '', manager: '' },
       departs: [
         {
@@ -59,6 +65,11 @@ export default {
       // this.departs = result.depts // 这里需要把数组转换为树形结构
       this.departs = tranListToTreeData(result.depts, '')
       // console.log(result)
+    },
+    addDepts(node) {
+      this.showDialog = true // 显示弹层
+      // 因为node是当前的点击的部门， 此时这个部门应该记录下来,
+      this.node = node
     }
   }
 }
