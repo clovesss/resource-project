9fa0be262b4ac730e02363d9e001f67707b055ca
clovesss <1214488973@qq.com>
2022-09-22 13:06:05 +0800

    获取组织架构数据，并进行树形处理

--- src/api/departments.js
+++ src/api/departments.js
@@ -8,3 +8,15 @@ export function getDepartments() {
     url: '/company/department'
   })
 }
+/**
+ *
+ * @description 删除组织架构的数据
+*/
+//  接口是根据restful的规则设计的
+// 删除 delete  新增 post  修改 put 获取 get
+export function delDepartments(id) {
+  return request({
+    url: `/company/department/${id}`,
+    method: 'delete'
+  })
+}
