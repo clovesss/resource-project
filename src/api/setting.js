97889fdb443009f6d7566fd1faa545903b0368f7
clovesss <1214488973@qq.com>
2022-09-25 15:55:03 +0800

    公司设置-公司信息页面完成

--- src/api/setting.js
+++ src/api/setting.js
@@ -10,3 +10,14 @@ export function getRoleList(params) {
     params
   })
 }
+
+/**
+ *
+ * @param {*} companyId
+ * @description 获取公司信息
+ */
+export function getCompanyInfo(companyId) {
+  return request({
+    url: `/company/${companyId}`
+  })
+}
--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,8 @@ const getters = {
   token: state => state.user.token,
   name: state => state.user.userInfo.username, // 建立用户名称的映射
   userId: state => state.user.userInfo.userId, // 建立用户id的映射
-  staffPhoto: state => state.user.userInfo.staffPhoto
+  staffPhoto: state => state.user.userInfo.staffPhoto,
+  companyId: state => state.user.userInfo.companyId // 建立对公司Id的快捷访问
+
 }
 export default getters
